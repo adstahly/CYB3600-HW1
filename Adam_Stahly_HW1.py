4195b9fe33c7679c45051cc81762fb8e73dc7edc
adstahly <adstahly@gmail.com>
2025-08-29 08:38:17 -0500

    Math Issues

--- Adam_Stahly_HW1.py
+++ Adam_Stahly_HW1.py
@@ -1,8 +1,11 @@
 import random
+import math
 
 plaintext = input("Enter the plaintext message: ")
 key_length = random.randint(4, 10)
-tmatrix = [len(plaintext)/key_length][key_length]
-for char in plaintext:
-    tmatrix[
+print(key_length)
+tmatrix = [math.ceil(float(len(plaintext)/key_length))][key_length]
+for i in range(len(plaintext)):
+    for j in range(len(plaintext)):
+        tmatrix[i][j] = plaintext[i]
 print(tmatrix)
