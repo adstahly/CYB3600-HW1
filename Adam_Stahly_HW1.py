848662b37fa4a45f087f0a6c4f7b2d37db7475c8
adstahly <adstahly@gmail.com>
2025-08-29 08:24:37 -0500

    Initial commit

---
+++ Adam_Stahly_HW1.py
@@ -0,0 +1,8 @@
+import random
+
+plaintext = input("Enter the plaintext message: ")
+key_length = random.randint(4, 10)
+tmatrix = [len(plaintext)/key_length][key_length]
+for char in plaintext:
+    tmatrix[]
+print(tmatrix)
