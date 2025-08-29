f57a6d439bccea62226036bb98c834311bf2e765
adstahly <adstahly@gmail.com>
2025-08-29 08:26:18 -0500

    Initial commit

--- Adam_Stahly_HW1.py
+++ Adam_Stahly_HW1.py
@@ -4,5 +4,5 @@ plaintext = input("Enter the plaintext message: ")
 key_length = random.randint(4, 10)
 tmatrix = [len(plaintext)/key_length][key_length]
 for char in plaintext:
-    tmatrix[]
+    tmatrix[
 print(tmatrix)
