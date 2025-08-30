24709cd676554b310f7ee2517be3addb27f76137
adstahly <adstahly@gmail.com>
2025-08-29 20:58:49 -0500

    Added Populate_Matrix

--- Adam_Stahly_HW1.py
+++ Adam_Stahly_HW1.py
@@ -1,25 +1,30 @@
 import random
 import math
 
+def populate_matrix(text,matrix):
+    row_counter = 0
+    for letter_index, char in enumerate(text):
+        current_row = row_counter % row_amount
+        current_col = letter_index % key_length
+        matrix[current_row][current_col] = char
+        if current_col == key_length - 1:
+            row_counter += 1
+    return matrix
+
 plaintext = input("Enter the plaintext message: ")
 key_length = random.randint(4, 10)
 motified_text = plaintext.replace(' ', '')
 row_amount = math.ceil(float(len(motified_text)/key_length))
 tmatrix = [[" " for _ in range(key_length)] for _ in range(row_amount)]
-row_counter = 0
-for letter_index, char in enumerate(motified_text):
-    current_row = row_counter % row_amount
-    current_col = letter_index % key_length
-    tmatrix[current_row][current_col] = char
-    if current_col == key_length-1:
-        row_counter += 1
+tmatrix = populate_matrix(motified_text, tmatrix)
 ciphertext = ""
+
 for col_index in range(key_length):
     for row_index in range(row_amount):
         ciphertext += tmatrix[row_index][col_index]
 for row in tmatrix:
     print(row)
-print(key_length)
-print("Plaintext:" + plaintext)
-print("Ciphertext:" + ciphertext.rstrip())
+print("Key Length: " + str(key_length))
+print("Plaintext: " + plaintext)
+print("Ciphertext: " + ciphertext.rstrip())
 
